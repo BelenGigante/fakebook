1e21951760a9162a197a8fe46c85b84f6aa78a38
BelenGigante <vinn_a@hotmail.com>
2023-02-02 22:21:42 -0500

    coded reactions model missing getter to format date

--- models/Reactions.js
+++ models/Reactions.js
@@ -1,14 +1,31 @@
 const { Types } = require('mongoose');
-const {Schema, model} = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const ReactionsSchema = new Schema(
     {
-        reactionsId:{
+        reactionsId: {
             type: Schema.Types.ObjectId,
-            default:()=> new Types.ObjectId()
+            default: () => new Types.ObjectId()
         },
     },
     {
-        
+        reactionsBody: {
+            type: String,
+            required: true,
+            maxlenght: 280,
+        },
+    },
+    {
+        username: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        //missing getter to format timestamp 
+        createdAt: {
+            type: Date,
+            default: Date.now,
+        },
     },
 )
