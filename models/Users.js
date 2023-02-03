4f6065a8d937660e86978b122b21f15ef327b1f5
BelenGigante <vinn_a@hotmail.com>
2023-02-02 21:49:52 -0500

    users model coded, not tested yet, match syntax to be fixed

--- models/Users.js
+++ models/Users.js
@@ -1,9 +1,42 @@
-const {Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 const usersSchema = new Schema(
     {
-
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true
+            //match: ^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$,
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thoughts'
+            }
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Users'
+            }
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        },
+        id: false
     }
 );
 
-const Users = model('Users',usersSchema)
+const Users = model('Users', usersSchema);
+
+module.exports = Users;
