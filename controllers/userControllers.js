b02e1886f35436370682d111cbbab44f346026df
BelenGigante <vinn_a@hotmail.com>
2023-02-02 23:34:50 -0500

    coded user controllers, put method not done,routes api  partially done

---
+++ controllers/userControllers.js
@@ -0,0 +1,65 @@
+const { ObjectId } = require('mongoose').Types;
+const { Users, Thoughts } = require('../models');
+
+//friend counter idk if its well done, check
+const friendCount = async () =>
+    Users.aggregate().count('friendCount')
+        .then((numberOfFriends) => numberOfFriends);
+
+module.exports = {
+    getUsers(req, res) {
+        Users.find().then(async (users) => {
+            const usersObj = {
+                users,
+                friendCount: await friendCount(),
+            };
+            return res.json(usersObj);
+        })
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+    getUsersById(req, res) {
+        Users.findOne({ _id: req.params.usersId })
+            .select('-__v').then(async (users) =>
+                !users
+                    ? res.status(404).json({ message: 'User not found' })
+                    : res.json({
+                        user,
+                    })
+            )
+            .catch((err) => {
+                console.log(err);
+                return res.status(500).json(err);
+            });
+    },
+    createUser(req, res) {
+        Users.create(req.body)
+            .then((users) => res.json(users))
+            .catch((err) => res.status(500).json(err));
+    },
+    //update user missing
+    deleteUser(req, res) {
+        Users.findByIdAndRemove({ __id: req.params.usersId })
+            .then((users) => !users ? res.status(404).json({ message: 'User not found' })
+            :Thoughts.findByIdAndUpdate(
+                {users: req.params.userId},
+                {$pull:{users: req.params.usersId}},
+                {new:true}
+            )
+        )
+        .then((thoughts)=>
+        !thoughts ? res.status(404).json({
+            message: 'User deleted, no thoughts found',
+        })
+        :res.json({meggare:'User deleted'})
+        )
+        .catch((err)=> {
+            console.log(err);
+            res.status(500).json(err);
+        });
+    },
+    
+
+}
